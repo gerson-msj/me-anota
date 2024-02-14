2dc5a8817ca5d17d53b015b09245f5caf14608b2
GersonMoraesSilva <gerson.junior-ext@viavarejo.com.br>
2024-02-13 21:30:33 -0300

    Correção de scripts

---
+++ web/scripts/js/Components/CriarComponent.js
@@ -0,0 +1,9 @@
+import BaseComponent from "./BaseComponent.js";
+export default class CriarComponent extends BaseComponent {
+    constructor() {
+        super("criar");
+    }
+    initialize() {
+    }
+}
+//# sourceMappingURL=CriarComponent.js.map
